66482a54cc1586e0de912bd0d2b1fbe5c0e88378
Stuart Swindells <github@lumo.me.uk>
2012-08-15 12:01:03 +0100

    Rename ConfigReader to something more descriptive, refactor getting a db connection into a separate method for better testability

--- aliases.py
+++ aliases.py
@@ -1,32 +1,37 @@
 #!/usr/bin/python
 
 import ConfigParser
-import ConfigReader
+import PostfixConfig
 from optparse import OptionParser, OptionGroup
 import MySQLdb, MySQLdb.cursors
 
 class Aliases:
-    def __init__(self):
+    def __init__(self):    
+        self.dbc = None
+
+    def get_dbc(self):
         self.config = ConfigParser.SafeConfigParser()
 
         self.config.read('config.ini')
-        self.aliases_file = self.config.get('Postfix Config', 'alias_file')
+        aliases_file = self.config.get('Postfix Config', 'alias_file')
+        self.config_reader = PostfixConfig.Reader(aliases_file)
     
-        user = ConfigReader.get_value(self.aliases_file, 'user')
+        user = self.config_reader.get_value('user')
         #host deliberately excluded - use localhost or go home
-        password = ConfigReader.get_value(self.aliases_file, 'password')
-        db_name = ConfigReader.get_value(self.aliases_file, 'dbname')
+        password = self.config_reader.get_value('password')
+        db_name = self.config_reader.get_value('dbname')
     
         self.dbc = MySQLdb.connect(
             user = user, passwd = password, db = db_name, 
-            cursorclass=MySQLdb.cursors.DictCursor)    
-    
-    
-    def get_aliases(self, alias='', dest=''):
-        table_name = ConfigReader.get_value(self.aliases_file, 'table')
-        alias_field = ConfigReader.get_value(self.aliases_file, 'where_field')
-        destination_field = ConfigReader.get_value(
-            self.aliases_file, 'select_field')
+            cursorclass=MySQLdb.cursors.DictCursor)
+        
+    def get_aliases(self, dest=None, alias=None):
+        if not self.dbc:
+            self.get_dbc()
+        
+        table_name = self.config_reader.get_value('table')
+        alias_field = self.config_reader.get_value('where_field')
+        destination_field = self.config_reader.get_value('select_field')
 
         query = "SELECT %s AS alias, %s AS destination FROM %s " \
         % (alias_field, destination_field, table_name)
@@ -35,23 +40,43 @@ class Aliases:
             query += "WHERE "
             if alias and len(alias) > 0:
                 query += "%s LIKE '%%%s%%' " % (alias_field, alias)
+            if alias and dest:
+                query += "AND "
             if dest and len(dest) > 0:
                 query += "%s LIKE '%%%s%%' " % (destination_field, dest)
         query += "ORDER BY %s ASC" % (alias_field)
         
+        print query
         cursor = self.dbc.cursor()
         cursor.execute(query)
         alias_list = cursor.fetchall()
         return alias_list
         
+    def set_alias(self, alias='', dest='', overwrite=False):
+        existing = self.get_aliases(alias=alias)
+        if len(existing) == 0:
+            table_name = self.config_reader.get_value('table')
+            alias_field = self.config_reader.get_value('where_field')
+            destination_field = self.config_reader.get_value('select_field')
+                
+            query = "INSERT INTO %s (%s, %s) VALUES (%s, %s)" \
+            % (table_name, alias_field, destination_field, alias, dest)
+            
+            print query
+        elif overwrite:
+            return
+        else:
+            print "Alias already exists!"
+            
+        
 def options(option_parser):
     option_parser.add_option("--show", action="store_true", 
         dest="show", help=
-        """List aliases. Alias and/or destination may be given 
-        but are optional.""", default=True)
+        ("(default) List aliases. Alias and/or destination may be given"
+        "but are optional."), default=True)
     option_parser.add_option("-i", "--insert", action="store_true", 
-        dest="insert", help="""Insert alias. 
-        An alias and its destination must be given.""")
+        dest="insert", help=("Insert alias. "
+        "An alias and its destination must be given."))
         
     params_group = OptionGroup(option_parser, "Values")
     params_group.add_option("-a", "--alias", 
@@ -62,8 +87,8 @@ def options(option_parser):
     
     (opts, args) = option_parser.parse_args()
     if opts.insert and not (opts.alias and opts.dest):
-        option_parser.error("""
-        Alias and destination are required to add an alias."""
+        option_parser.error(
+        ("Alias and destination are required to add an alias.")
         )
     
     return opts
@@ -73,7 +98,7 @@ def main():
     reader = Aliases()
     
     if opt.show:
-        aliases = reader.get_aliases(opt.alias, opt.dest)
+        aliases = reader.get_aliases(dest = opt.dest, alias = opt.alias)
         for alias in aliases:
             print alias['alias'], alias['destination']
         
