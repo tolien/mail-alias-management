6c0ec6bca043e743f73e4d053a099c4144de9c87
Stuart Swindells <github@lumo.me.uk>
2012-09-07 15:01:17 +0100

    Move test data for each class into its own dir

--- PostfixConfig_Test.py
+++ PostfixConfig_Test.py
@@ -2,26 +2,34 @@
 
 import unittest
 import PostfixConfig
+import os
 
 class TestPostfixConfigReader(unittest.TestCase):
+    def setUp(self):
+        self.test_data_root = 'test_data/PostfixConfig/'
+        os.chmod(self.test_data_root + 'inaccessable-aliases.cf', 0000)
+    
+    def tearDown(self):
+        os.chmod(self.test_data_root + 'inaccessable-aliases.cf', 0700)
+        
     def test_file_not_exist(self):
         reader = PostfixConfig.Reader('file-which-does-not-exist')
         self.assertRaises(IOError, reader.get_value, 'foo')
         
     def test_file_no_permission(self):
-        reader = PostfixConfig.Reader('test_data/mysql-aliases.cf')
+        reader = PostfixConfig.Reader(self.test_data_root + 'inaccessable-aliases.cf')
         self.assertRaises(PostfixConfig.NoPermissionException, reader.get_value, 'foo')
 
     def test_key_not_existing(self):
-        reader = PostfixConfig.Reader('test_data/accessable-aliases.cf')
+        reader = PostfixConfig.Reader(self.test_data_root + 'accessable-aliases.cf')
         self.assertRaises(KeyError, reader.get_value, 'foo')
 
     def test_key_existing(self):
-        reader = PostfixConfig.Reader('test_data/accessable-aliases.cf')
+        reader = PostfixConfig.Reader(self.test_data_root + 'accessable-aliases.cf')
         self.assertEqual(reader.get_value('where_field'), 'alias')
         
     def test_extra_equals(self):
-        reader = PostfixConfig.Reader('test_data/extra-equals.cf')
+        reader = PostfixConfig.Reader(self.test_data_root + 'extra-equals.cf')
         self.assertEqual(reader.get_value('select_field'), 'destination = cheese')
 
 if __name__ == "__main__":
