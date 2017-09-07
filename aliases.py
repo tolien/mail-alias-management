90d8ec86dc21057298b05862a708303cea82e055
Stuart Swindells <github@lumo.me.uk>
2017-09-07 21:07:00 +0000

    Update to use Python 3 Fix a bunch of pylint warnings - 9.07/10 now

--- aliases.py
+++ aliases.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 """Aliases
 Accessor
@@ -8,47 +8,48 @@ read_values: returns all config for a file
 get_value: returns a specific config item for a file
 """
 
-import ConfigParser
-import PostfixConfig
 from argparse import ArgumentParser
-import MySQLdb, MySQLdb.cursors
+import configparser
+import MySQLdb
+import MySQLdb.cursors
+import PostfixConfig
 
 class Aliases:
-    def __init__(self):    
+    def __init__(self):
         self.dbc = None
         self.config_file = 'config.ini'
         self.config_reader = None
 
     def config_parser(self):
-        config = ConfigParser.SafeConfigParser()
+        config = configparser.ConfigParser()
 
         config.read(self.config_file)
         aliases_file = config.get('Postfix Config', 'alias_file')
         self.config_reader = PostfixConfig.Reader(aliases_file)
-    
+
     def get_dbc(self):
-        if self.config_reader == None:
+        if self.config_reader is None:
             self.config_parser()
         user = self.config_reader.get_value('user')
         #host deliberately excluded - use localhost or go home
         password = self.config_reader.get_value('password')
         db_name = self.config_reader.get_value('dbname')
-    
+
         self.dbc = MySQLdb.connect(
-            user = user, passwd = password, db = db_name, 
+            user=user, passwd=password, db=db_name,
             cursorclass=MySQLdb.cursors.DictCursor)
-        
+
     def get_aliases(self, dest=None, alias=None):
         if not self.dbc:
             self.get_dbc()
-        
+
         table_name = self.config_reader.get_value('table')
         alias_field = self.config_reader.get_value('where_field')
         destination_field = self.config_reader.get_value('select_field')
 
         query = "SELECT %s AS alias, %s AS destination FROM %s " \
         % (alias_field, destination_field, table_name)
-        
+
         if alias or dest:
             query += "WHERE "
             if alias and len(alias) > 0:
@@ -58,12 +59,12 @@ class Aliases:
             if dest and len(dest) > 0:
                 query += "%s LIKE '%%%s%%' " % (destination_field, dest)
         query += "ORDER BY %s ASC" % (alias_field)
-        
+
         cursor = self.dbc.cursor()
         cursor.execute(query)
         alias_list = cursor.fetchall()
         return alias_list
-        
+
     def delete_alias(self, alias):
         aliases = self.get_aliases(alias=alias)
         if len(aliases) == 0:
@@ -75,18 +76,18 @@ class Aliases:
             cursor = self.dbc.cursor()
             cursor.execute(query)
         else:
-            raise NameError("Alias matched multiple records")            
-        
+            raise NameError("Alias matched multiple records")
+
     def insert_alias(self, alias='', dest='', overwrite=False):
         existing = self.get_aliases(alias=alias)
         if len(existing) == 0:
             table_name = self.config_reader.get_value('table')
             alias_field = self.config_reader.get_value('where_field')
             destination_field = self.config_reader.get_value('select_field')
-                
+
             query = "INSERT INTO %s (%s, %s) VALUES ('%s', '%s')" \
             % (table_name, alias_field, destination_field, alias, dest)
-            
+
             cursor = self.dbc.cursor()
             cursor.execute(query)
 
@@ -95,55 +96,55 @@ class Aliases:
             self.insert_alias(alias, dest, overwrite)
         else:
             raise NameError("Alias already exists!")
-            
-        
+
+
 def options(parser):
     mutex_group = parser.add_mutually_exclusive_group()
     mutex_group.add_argument("-s", "--show", action="store_const",
        const="show", dest="action", help=
             ("(default) List aliases. Alias and/or destination may be given"
-            "but are optional."), default=True)
-    mutex_group.add_argument("-i", "--insert", action="store_const", 
+             "but are optional."), default=True)
+    mutex_group.add_argument("-i", "--insert", action="store_const",
         const="insert", dest="action", help=("Insert alias. "
             "An alias and its destination must be given."))
     mutex_group.add_argument("--delete", action="store_const",
         const="delete", dest="action", help="Delete alias.")
-        
+
     params_group = parser.add_argument_group("Values")
-    params_group.add_argument("-a", "--alias", 
+    params_group.add_argument("-a", "--alias",
         dest="alias", help="Alias fragment.")
     params_group.add_argument("-d", "--destination",
         dest="dest", help="Destination fragment")
-    
+
     parser.set_defaults(action='show')
     opts = parser.parse_args()
     if opts.action == 'insert' and not (opts.alias and opts.dest):
         parser.error(
-        ("Alias and destination are required to add an alias.")
+            ("Alias and destination are required to add an alias.")
         )
-    elif opts.action == 'delete' and not (opts.alias):
+    elif opts.action == 'delete' and not opts.alias:
         parser.error(
             ("An alias must be specified.")
         )
-    
+
     return opts
-    
+
 def main():
     opt = options(ArgumentParser())
     reader = Aliases()
 
     try:
         if opt.action == 'show':
-            aliases = reader.get_aliases(alias = opt.alias, dest=opt.dest)
+            aliases = reader.get_aliases(alias=opt.alias, dest=opt.dest)
             for alias in aliases:
-                print alias['alias'], '->', alias['destination']
+                print(alias['alias'], '->', alias['destination'])
         elif opt.action == 'insert':
             aliases = reader.insert_alias(dest=opt.dest, alias=opt.alias)
         elif opt.action == 'delete':
             reader.delete_alias(alias=opt.alias)
-    except PostfixConfig.NoPermissionException, exception:
-        print "No permission to read %s" % exception.get_filename()
-        
-     
+    except PostfixConfig.NoPermissionException as exception:
+        print("No permission to read %s" % exception.get_filename())
+
+
 if __name__ == "__main__":
     main()
